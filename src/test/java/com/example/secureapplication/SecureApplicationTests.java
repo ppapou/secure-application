fc3ae923e5b391ee7fe235410d83762e45994f0f
Pavel Papou <Pavel_Papou@epam.com>
2023-09-21 14:10:26 -0400

    Application structure
     - src folder and gradle files are added

---
+++ src/test/java/com/example/secureapplication/SecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
