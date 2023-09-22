dccd06e25fe7a11c4b95087619eb17cce30a8017
Pavel Papou <Pavel_Papou@epam.com>
2023-09-22 16:26:29 -0400

    Basic authorisation has added

---
+++ src/main/java/com/example/secureapplication/config/InMemoryUserDetailsManager.java
@@ -0,0 +1,10 @@
+package com.example.secureapplication.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+//TODO:implement persistent mechanism
+public class InMemoryUserDetailsManager {
+
+
+    public InMemoryUserDetailsManager(UserDetails user1, UserDetails user2, UserDetails admin) {
+    }
+}
---
+++ src/main/java/com/example/secureapplication/config/SecSecurityConfig.java
@@ -0,0 +1,91 @@
+package com.example.secureapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("user2Pass"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ /**
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .antMatchers("/anonymous*")
+                .anonymous()
+                .antMatchers("/login*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/homepage.html", true)
+                .failureUrl("/login.html?error=true")
+                .failureHandler(authenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(logoutSuccessHandler());
+
+        return http.build();
+    }
+*/
+
+    private Object logoutSuccessHandler() {
+        return null;
+    }
+
+    private Object authenticationFailureHandler() {
+        return null;
+    }
+
+
+
+}
